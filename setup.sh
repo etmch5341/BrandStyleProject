298ed9aa7a09307cb59264f883c2e21ad31a8475
etmch5341 <etmch5341@gmail.com>
2026-04-08 01:33:28 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-sed -i '' 's/from torchvision.transforms.functional_tensor import rgb_to_grayscale/from torchvision.transforms.functional import rgb_to_grayscale/' /Users/ethanchandra/Documents/GitHub/BrandStyleProject/.venv/lib/python3.12/site-packages/basicsr/data/degradations.py
+sed -i '' 's/from torchvision.transforms.functional_tensor import rgb_to_grayscale/from torchvision.transforms.functional import rgb_to_grayscale/' ~/brand-gen-env/lib/python3.12/site-packages/basicsr/data/degradations.py
 python3 -c "import basicsr; print('basicsr ok')"
 python3 -c "import realesrgan; print('realesrgan ok')"
